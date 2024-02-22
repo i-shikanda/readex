58379be3ef051f8d0a18028197fd78e3a02096f0
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2024-02-22 14:06:57 +0300

    update new arrivals

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -2,28 +2,59 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="py-8 px-12 bg-[#ffeddf] mt-28 pt-12">
-      <h1 className="flex">
-        <span className="text-4xl font-semibold flex w-1/2">
-          About Redex Library.
-        </span>
-        <span className="flex w-1/2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          distinctio pariatur atque sint at dicta ut error quia qui maiores.
-        </span>
-      </h1>
-      <div className="flex mt-8 bg-[#fbb7c6] rounded-xl">
-        <Image
-          src="/about_img.png"
-          alt="About Image"
-          width={600}
-          height={600}
-          className="w-1/2"
-        />
-        <div className="w-1/2 bg-white rounded-xl flex items-center justify-center m-6">
-          <h1 className="text-4xl font-semibold">
-            The Redex Library.
-          </h1>
+    <div>
+      <div className="py-8 px-12 bg-[#ffeddf] mt-28 pt-12">
+        <h1 className="flex">
+          <span className="text-4xl font-semibold flex w-1/2">
+            About Redex Library.
+          </span>
+          <span className="flex w-1/2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            distinctio pariatur atque sint at dicta ut error quia qui maiores.
+          </span>
+        </h1>
+        <div className="flex mt-8 bg-[#fbb7c6] rounded-xl">
+          <Image
+            src="/about_img.png"
+            alt="About Image"
+            width={600}
+            height={600}
+            className="w-1/2"
+          />
+          <div className="w-1/2 bg-white rounded-xl flex items-center justify-center m-6">
+            <h1 className="text-4xl font-semibold">The Redex Library.</h1>
+            <div>
+              <div className="flex flex-col">
+              <div>
+                  {/* A persons Icon */}
+                  <div>
+                    <h1 className="font-medium text-gray-700">
+                      Author
+                    </h1>
+                    <p className="font-bold">
+                      Redex Library
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  {/* A PDF Icon */}
+                  <div>
+                    <h1 className="font-medium text-gray-700">
+                      Total
+                    </h1>
+                    <p className="font-bold">
+                      368 Books
+                    </p>
+                  </div>
+                </div>
+                
+              </div>
+              <div className="flex justify-between items-center">
+                <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+                <div className="w-24 h-24 bg-blue-500 rounded-full"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/sections/newarrivals.tsx
+++ src/app/components/sections/newarrivals.tsx
@@ -1,11 +1,106 @@
+import Image from "next/image";
+
 export default function NewArrivals() {
   return (
-    <div>
-      <h1>New Arrivals</h1>
-      <p>
-        This is a simple example of a React application with a few pages and
-        components.
-      </p>
-    </div>
+    <section className="flex items-center bg-slate-100 py-10 flex-col w-full">
+      <div className="w-[75%]">
+        <header className="flex justify-between w-full p-20">
+          <div className="w-[75%]">
+            <h1 className="text-3xl font-bold">New Arrivals</h1>
+            <p className="font-medium text-xl">
+              Find our top categories that will help you with what you are
+              looking for
+            </p>
+          </div>
+          <div className="flex space-x-2 w-[25%]">
+            <div className="rounded-full h-12 w-12 border-2 border-blue-400"></div>
+            <div className="rounded-full h-12 w-12 border-2 border-blue-400 bg-blue-400"></div>
+          </div>
+        </header>
+        <div className="flex space-x-8 justify-center bg-white shadow py-10 px-40 w-[100%] rounded-3xl">
+          <div className="rounded-3xl p-4 bg-orange-300">
+            <div>
+              <Image
+                src="/newarrivals_img.png"
+                alt="New Arrivals Image"
+                width={600}
+                height={600}
+                width={600}
+                className="w-1/2"
+                alt="New Arrivals Image"
+              />
+            </div>
+            <div className="bg-white rounded-3xl p-4">
+              <div>
+                <div className="flex justify-between">
+                  <h1 className="font-bold text-xl">Horlicks</h1>
+                  <p className="text-orange-500 font-medium">$34.00</p>
+                </div>
+                <p className="font-medium">Sweet molted milk hot drink</p>
+              </div>
+              <div>
+                <button className="rounded-xl bg-transparent border-2 px-4 py-1 border-orange-400 font-medium">
+                  View Book Demo
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="rounded-3xl p-4 bg-orange-300">
+            <div>
+              <Image
+                src="/newarrivals_img.png"
+                alt="New Arrivals Image"
+                width={600}
+                height={600}
+                width={600}
+                className="w-1/2"
+                alt="New Arrivals Image"
+              />
+            </div>
+            <div className="bg-white rounded-3xl p-4">
+              <div>
+                <div className="flex justify-between">
+                  <h1 className="font-bold text-xl">Horlicks</h1>
+                  <p className="text-orange-500 font-medium">$34.00</p>
+                </div>
+                <p className="font-medium">Sweet molted milk hot drink</p>
+              </div>
+              <div>
+                <button className="rounded-xl bg-transparent border-2 px-4 py-1 border-orange-400 font-medium">
+                  View Book Demo
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="rounded-3xl p-4 bg-orange-300">
+            <div>
+              <Image
+                src="/newarrivals_img.png"
+                alt="New Arrivals Image"
+                width={600}
+                height={600}
+                width={600}
+                className="w-1/2"
+                alt="New Arrivals Image"
+              />
+            </div>
+            <div className="bg-white rounded-3xl p-4">
+              <div>
+                <div className="flex justify-between">
+                  <h1 className="font-bold text-xl">Horlicks</h1>
+                  <p className="text-orange-500 font-medium">$34.00</p>
+                </div>
+                <p className="font-medium">Sweet molted milk hot drink</p>
+              </div>
+              <div>
+                <button className="rounded-xl bg-transparent border-2 px-4 py-1 border-orange-400 font-medium">
+                  View Book Demo
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
